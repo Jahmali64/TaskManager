1caff30d5b8430deacff2dd3edd6ade696c201ea
Jahmali Mcknight <57546416+Jahmali64@users.noreply.github.com>
2026-03-18 22:55:30 -0700

    remove comment

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -54,8 +54,7 @@ public static class Program
             builder.Services.AddOpenApi();
 
             WebApplication app = builder.Build();
-
-            // Configure the HTTP request pipeline.
+            
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
